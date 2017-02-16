a51b268e3f67feb0458f4055b3ef6bdcf7612faf
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-02-16 11:58:40 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'sinatra-flash'
+gem 'sinatra'
+gem 'sqlite3'
+gem 'rake'
---
+++ main.rb
@@ -0,0 +1,97 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './models'
+require 'sinatra/flash'
+
+set :database, "sqlite3:test.sqlite3"
+enable :sessions
+
+get '/' do 
+	@users = User.all
+	@posts = Post.all
+	@user = User.find(session[:user_id]) if session[:user_id]
+
+	session[:visited] = true
+
+	p session[:visited] = "I'm Here!"
+
+	erb :home
+end
+
+
+
+post '/posts' do
+	params.inspect
+	post = Post.new
+
+	post.title = params["title"]
+	post.body = params["body"]
+
+	post.save
+
+	redirect '/'
+end
+
+get "/posts/delete/:id" do
+	post = Post.find(params[:id])
+	post.destroy
+
+	redirect "/"
+end 
+
+get "/posts/:id" do
+	@post = Post.find(params[:id])
+
+	erb :post_show
+end 
+
+get "/posts/edit/:id" do
+	@post = Post.find(params[:id])
+
+	erb :post_edit
+end 
+
+post "/posts/edit/:id" do
+	@post = Post.find(params[:id])
+
+	@post.title = params[:title]
+	@post.body = params[:body]
+
+	@post.save
+
+	redirect "/posts/#{@post.id}"
+end
+
+get "/sign_in" do
+	erb :sign_in
+end
+
+post "/sessions/new" do
+	user = User.where(email: params[:email]).first
+if user
+
+	session[:user_id] = user.id
+	flash[:notice] = "You've successfully signed in"
+end
+
+	redirect "/"
+end
+
+get "/sign-out" do
+	session.clear
+	flash[:notice] = "You've successfully signed out!"
+
+	redirect "/"
+end
+
+
+
+
+
+
+
+
+
+
+
+
